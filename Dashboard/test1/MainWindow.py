d91c58a3fe58926a20d4a73fc0d8b7f838b30b1d
vermale <7k@laposte.net>
2024-12-01 21:05:50 +0100

    ok

--- Dashboard/test1/MainWindow.py
+++ Dashboard/test1/MainWindow.py
@@ -3,17 +3,16 @@ from PyQt5.QtCore import QObject, pyqtSignal, QEvent, QTimer, QSize
 from PyQt5.QtGui import QIcon, QPixmap, QColor
 import os
 
-from pylab import close, figure, get_current_fig_manager, plot, xlabel, ylabel, title, grid, connect, show, ioff
+from matplotlib.pyplot import close, figure, get_current_fig_manager, plot, xlabel, ylabel, title, grid, connect, show, ioff
 from ui_mainwindow import Ui_MainWindow
 import sys, os, glob
 from Dial import Dial
-import serial
-#import CanProtocol
 
 import numpy
 import can
 
 import time
+import csv
 
 from matplotlib import pyplot
 from string import Template
@@ -29,6 +28,10 @@ Fuel = 0.0
 Rpm = 0
 Tps = 0
 
+os.system("sudo /sbin/ip link set down can0")
+os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
+time.sleep(0.1)
+
 def decodeData(message):
 
 	global Temp 
@@ -40,122 +43,48 @@ def decodeData(message):
 	global Fuel 
 	global Rpm 
 	global Tps 
-	
+
 	message = str(message)
 	canid = message[41:44]
-	mess = message[69:].replace(' ', '' )
+	mess = message[78:]
 	message = canid+"#"+mess
-	
+    
 	data = message[4:]
-	#print("mess:", message, "canid:",canid," message:",data)
+	print("mess:", message, "canid:",canid," message:",data)
 	if ( canid == '300' ):
-		val = float(int(data[8:10],16)*256+int(data[10:12],16))
+		val = float(int(data[12:13],8)*256+int(data[15:16],8))
 		Map = val/1000
-		val = int(data[0:2],16)*256+int(data[2:4],16)
+		val = int(data[0:1],8)*256+int(data[3:4],8)
 		Rpm = val
-		val = int(data[4:6],16)*100/255
+		val = int(data[6:7],8)*100/255
 		Tps = val
 	if ( canid == '301' ):
-		Lambda2 = float(int(data[4:6],16))*2*14.7/255
+		Lambda2 = float(int(data[6:7],8))*2*14.7/255
 	if ( canid == '305' ):
-		Lambda1=  float(int(data[0:2],16))*2*14.7/255
+		Lambda1=  float(int(data[0:1],8))*2*14.7/255
 	if ( canid == '306' ):
-		Fuel = float(int(data[12:14],16)*256+int(data[14:16],16))*0.1
+		Fuel = float(int(data[18:19],8)*256+int(data[21:22],8))*0.1
 	if ( canid == '308'):
 		val = 0.0
-		val = float(int(data[0:2],16)*256+int(data[2:4],16))
+		val = float(int(data[0:1],8)*256+int(data[3:4],8))
 		Volt = val
-		Volt = Volt*18/1000
+		Volt = Volt*18/1023
 	if ( canid == '30b'):
-		Temp = float((int(data[0:2],16)*160))/255-10
-		Air = float((int(data[6:8],16)*160))/255-10
+		Temp = float((int(data[0:1],8)))*150/255-10
+		Air = float((int(data[9:10],8)))*150/255-10
 
 			
 def can_rx_task():
-	bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
-	while True:
-		message = bus.recv()
-		decodeData(message)
+    bus = can.interface.Bus(channel='can0', bustype='socketcan', bitrate=500000)
+    print("init")     
+    while True:
+        message = bus.recv(24)
+        decodeData(message)
+        time.sleep(0.001)
 
 t = Thread(target = can_rx_task)	# Start receive thread
 t.start()
 			
-
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-    port='/dev/ttyACM0',
-    baudrate=9600,
-    parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.SEVENBITS
-)
-ser.isOpen()
-
-
-def getTime(string,format,returnFormat):
-        return time.strftime(returnFormat, time.strptime(string, format)) # Convert date and time to a nice printable format
-
-def getLatLng(latString,lngString):
-        #print(latString, lngString)
-        lat = ""
-        lng = ""
-        if (len(latString) > 0 ):
-               lat = latString[:2].lstrip('0') + "." + "%.7s" % str(float(latString[2:])*1.0/60.0).lstrip("0.")
-               lng = lngString[:3].lstrip('0') + "." + "%.7s" % str(float(lngString[3:])*1.0/60.0).lstrip("0.")
-        return(lat,lng)
-
-def decode(lines):
-        print("Time:", lines[1][0:2]+":"+lines[1][2:4]+":"+lines[1][4:6])
-        print("Status (A=OK,V=KO):", lines[2])
-        latlng = getLatLng(lines[3],lines[5])
-        print("Lat,Long: ", latlng[0], lines[4], ", ", latlng[1], lines[6])
-        print("Speed (knots):", lines[7])
-
-def getGpsTime(lines):
-        return(lines[1][0:2]+":"+lines[1][2:4]+":"+lines[1][4:6])
-        
-def getGpsLat(lines):
-        return(lines[3])        
-        
-def getGpsLong(lines):
-        return(lines[5])        
-
-def getGpsSpeed(lines):
-		  ch = lines[7]
-		  if ( ch == ''):
-		  	   ch = '0'
-		  km = float(ch)*1.852	   
-		  return(int(km))
-
-
-def checksum(line):
-   checkString = line.partition("*")
-   checksum = 0
-   for c in checkString[0]:
-      checksum ^= ord(c)
-
-   try: # Just to make sure
-      inputChecksum = int(checkString[2].rstrip(), 16);
-   except:
-      print("Error in string")
-      return False
-
-   if checksum == inputChecksum:
-      return True
-   else:
-      print("=====================================================================================")
-      print("===================================Checksum error!===================================")
-      print("=====================================================================================")
-      print(hex(checksum), "!=", hex(inputChecksum))
-      return False
-
-def readString():
-        while 1:
-                while ser.read().decode("utf-8") != '$': # Wait for the begging of the string
-                      pass # Do nothing
-                line = ser.readline().decode("utf-8") # Read the entire string
-                return line             
-
 def clickable(widget):
       
     class Filter(QObject):
@@ -176,67 +105,6 @@ def clickable(widget):
     return tfilter.clicked
     
     
-class DigitalSpeed(QLCDNumber):
-
-    def __init__(self, parent=None):
-        super(DigitalSpeed, self).__init__(parent)
-        self.setSegmentStyle(QLCDNumber.Filled)
-        self.showTime("152")
-        self.resize(200, 40)     
-
-    def showTime(self,texte):
-        self.display(texte)
-          
-class GpsPoint():
-	
-    latitude = 0
-    longitude = 0
-	
-    def __init__(self, Lat=0, Long= 0):	
-        latitude = Lat
-        longitude = Long
-	
-    def GpsPoint():
-        latitude = 0.0
-        longitude = 0.0
-	   
-    def GpsPoint(Lat, Long):
-        latitude = Lat
-        longitude = Long
-
-           
-class Track():
-	
-	 #43.7112163,4.011291
-	 #43.7112131,4.0025198
-	 
-    finishLinePoint1 = GpsPoint(43.7112163,4.011291)
-    finishLinePoint2 = GpsPoint(43.7112131,4.0025198)
-    
-
-    def isFinishLinePassed(self,start, finish):
-		
-        delta0 = (self.finishLinePoint1.latitude - self.finishLinePoint2.latitude) * (finish.longitude - start.longitude)
-        delta0 = delta0 -(self.finishLinePoint1.longitude - self.finishLinePoint2.longitude) * (finish.latitude - start.latitude)
-        if (delta0 == 0.0 ):
-             return(0)
-        delta1 = (self.finishLinePoint1.longitude - self.finishLinePoint2.longitude) * (start.latitude - self.finishLinePoint2.latitude)
-        delta1 = delta1 -(self.finishLinePoint1.latitude - self.finishLinePoint2.latitude) * (start.longitude - self.finishLinePoint2.longitude)	
-        delta2 = (finish.longitude - start.longitude) * (start.latitude - self.finishLinePoint2.latitude)
-        delta2 = delta2 -(finish.latitude - start.latitude) * (start.longitude - self.finishLinePoint2.longitude)
-
-        ka = delta1/delta0
-        kb = delta2/delta0
-		
-        if (ka < 0 or ka > 1 or kb < 0 or kb > 1):
-      	  return(0)
-      
-        Long = start.longitude + ka * (finish.longitude - start.longitude)
-        Lat = start.latitude + ka * (finish.latitude - start.latitude)
-
-        intersection = GpsPoint(Lat, Long);
-        return(1)         
-		  
 	  
 class MainWindow(QMainWindow):
     
@@ -253,8 +121,6 @@ class MainWindow(QMainWindow):
     TempWin = pyplot
     #tool = CanProtocol.CanTool()
     oldMess = ""
-    myTrack = Track() 
-    start = GpsPoint(0,0)
     
     
     def myShutDown(self):
@@ -267,8 +133,7 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.currentport = ""
-          
-        
+        self.showFullScreen()
         layout = QGridLayout(self.ui.centralwidget)
         layout.setContentsMargins(0,0,0,0)
         layout.setColumnMinimumWidth(1,1)
@@ -288,7 +153,7 @@ class MainWindow(QMainWindow):
         self.file = ''
         
         Quit = self.addPushB( 0, 'linux.jpg', layout)
-        Quit.clicked.connect(exit)
+        Quit.clicked.connect(self.close)
         motor = self.addPushB( 2, 'motor.jpg', layout)
         speed = self.addPushB( 3, 'speed.jpg', layout)   
         #speed.clicked.connect( layout.setEnabled(false))
@@ -341,11 +206,6 @@ class MainWindow(QMainWindow):
         self.meters.append(tps)
         layout.addWidget(tps, 3, 8, 3, 3 )
         
-        km = DigitalSpeed()
-        clickable(km).connect(self.graphKm)
-        self.meters.append(km)
-        layout.addWidget(km, 0, 5, 1, 3 ) 
-
         self.BoostList.append(0)
         self.BoostList.append(2)
         self.AfrList.append(0)
@@ -364,13 +224,11 @@ class MainWindow(QMainWindow):
         self.RpmList.append(7500)
         self.KmList.append(0)
         self.KmList.append(200)
-        
 
         #filter = [{'can_id':0x300, 'can_mask': 0x00f }]
         #bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
 
         #self.showFullScreen()
-        print("start")
         QTimer.singleShot(50,self.increment)
        
     def addPushB(self,pos,icon, layout):
@@ -457,20 +315,38 @@ class MainWindow(QMainWindow):
         self.graphWin('events', 'TPS', 'Thottle body%')
 
 
+    def write_to_csv(self, filename, data_arrays, headers):
+        with open(filename, 'w', newline='') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(headers)
+            csv_writer.writerows(zip(*data_arrays))
+
+    def fileWrite(self, data, name):
+
+        try:
+            f = open(name, 'w+')
+            for line in data:
+                f.writelines(str(line)+'\n')
+            f.close()    
+                
+        except:
+            print("Error writing file")    
+
 
     def saveAll(self):
 
+        headers = ['Rpm', 'Tps', 'Temp', 'Boost', 'Afr', 'Volt', 'Air','Fuel']
+        self.write_to_csv('my_data.csv', [self.RpmList, self.TpsList, self.TempList, self.BoostList, self.AfrList, self.VoltList, self.AirList, self.FuelList], headers)
         print("save")
-        self.fileWrite(self.TempList, "temp.txt") 
-        self.fileWrite(self.BoostList, "boost.txt") 
-        self.fileWrite(self.AirList, "air.txt") 
-        self.fileWrite(self.AfrList, "afr.txt") 
-        self.fileWrite(self.RpmList, "rpm.txt") 
-        self.fileWrite(self.TpsList, "tps.txt") 
-        self.fileWrite(self.FuelList, "fuel.txt") 
-        self.fileWrite(self.BattList, "batt.txt") 
+        #self.fileWrite(self.TempList, "temp.txt") 
+        #self.fileWrite(self.BoostList, "boost.txt") 
+        #self.fileWrite(self.AirList, "air.txt") 
+        #self.fileWrite(self.AfrList, "afr.txt") 
+        #self.fileWrite(self.RpmList, "rpm.txt") 
+        #self.fileWrite(self.TpsList, "tps.txt") 
+        #self.fileWrite(self.FuelList, "fuel.txt") 
+        #self.fileWrite(self.BattList, "batt.txt") 
         return
-
 
     def openVE(self):
         self.vewindow.show()
@@ -492,7 +368,6 @@ class MainWindow(QMainWindow):
         global Fuel 
         global Rpm 
         global Tps 
-
         
         batt = self.meters[2]
         batt.setSpeed(Volt)
@@ -522,7 +397,5 @@ class MainWindow(QMainWindow):
         rpm = self.meters[5]
         rpm.setSpeed(Rpm)
         self.RpmList.append(Rpm)
-        
-        
         
         QTimer.singleShot(50, self.increment)
