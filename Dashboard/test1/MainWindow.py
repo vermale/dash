188cb0c24c36cc008947c0f6d3083c67124cc9e7
vermale <7k@laposte.net>
2024-12-03 17:12:13 +0100

    cve

--- Dashboard/test1/MainWindow.py
+++ Dashboard/test1/MainWindow.py
@@ -27,10 +27,17 @@ Air = 0.0
 Fuel = 0.0 
 Rpm = 0
 Tps = 0
+dataList = []
 
 os.system("sudo /sbin/ip link set down can0")
 os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
 time.sleep(0.1)
+
+headers = ['Rpm', 'Tps', 'Temp', 'Boost', 'Afr', 'Volt', 'Air','Fuel']
+csvfile =  open('my_data.csv', 'a', newline='') 
+csv_writer = csv.writer(csvfile)
+csv_writer.writerow(headers)
+
 
 def decodeData(message):
 
@@ -52,26 +59,46 @@ def decodeData(message):
 	data = message[4:]
 	print("mess:", message, "canid:",canid," message:",data)
 	if ( canid == '300' ):
-		val = float(int(data[12:13],8)*256+int(data[15:16],8))
+		val = float(int(data[12:14],16)*256+int(data[15:17],16))
 		Map = val/1000
-		val = int(data[0:1],8)*256+int(data[3:4],8)
+		val = int(data[0:2],16)*256+int(data[3:5],16)
 		Rpm = val
-		val = int(data[6:7],8)*100/255
+		print("rpm=", Rpm)
+		val = int(data[6:8],16)*100/255
 		Tps = val
 	if ( canid == '301' ):
-		Lambda2 = float(int(data[6:7],8))*2*14.7/255
+		Lambda2 = float(int(data[6:8],16))*2*14.7/255
 	if ( canid == '305' ):
-		Lambda1=  float(int(data[0:1],8))*2*14.7/255
+		Lambda1=  int(data[0:2],16)*2*14.7/255
 	if ( canid == '306' ):
-		Fuel = float(int(data[18:19],8)*256+int(data[21:22],8))*0.1
+		Fuel = float(int(data[18:20],16)*256+int(data[21:23],16))*0.1
 	if ( canid == '308'):
 		val = 0.0
-		val = float(int(data[0:1],8)*256+int(data[3:4],8))
+		val = int(data[0:2],16)*256+int(data[3:5],16)
 		Volt = val
 		Volt = Volt*18/1023
 	if ( canid == '30b'):
-		Temp = float((int(data[0:1],8)))*150/255-10
-		Air = float((int(data[9:10],8)))*150/255-10
+		Temp = float((int(data[0:2],16)))*150/255-10
+		Air = float((int(data[9:11],16)))*150/255-10
+
+	
+def task():
+	dataList.append(format(Rpm,'.2f'))
+	dataList.append(format(Tps,'.2f'))
+	dataList.append(format(Temp,'.2f'))
+	dataList.append(format(Map,'.2f'))
+	dataList.append(format(Lambda1,'.2f'))
+	dataList.append(format(Volt,'.2f'))
+	dataList.append(format(Air,'.2f'))
+	dataList.append(format(Fuel,'.2f'))
+	csv_writer.writerow(dataList)
+	csvfile.flush()
+	dataList.clear()
+	
+
+while True:
+    task()
+    time.sleep(1)
 
 			
 def can_rx_task():
@@ -315,11 +342,11 @@ class MainWindow(QMainWindow):
         self.graphWin('events', 'TPS', 'Thottle body%')
 
 
-    def write_to_csv(self, filename, data_arrays, headers):
+    def write_to_csv(self, filename, headers):
         with open(filename, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerow(headers)
-            csv_writer.writerows(zip(*data_arrays))
+            writer.writerows(lignes)    #csv_writer.writerows(zip(*data_arrays))
 
     def fileWrite(self, data, name):
 
@@ -336,7 +363,7 @@ class MainWindow(QMainWindow):
     def saveAll(self):
 
         headers = ['Rpm', 'Tps', 'Temp', 'Boost', 'Afr', 'Volt', 'Air','Fuel']
-        self.write_to_csv('my_data.csv', [self.RpmList, self.TpsList, self.TempList, self.BoostList, self.AfrList, self.VoltList, self.AirList, self.FuelList], headers)
+        self.write_to_csv('my_data.csv', [self.RpmList, self.TpsList, self.TempList, self.BoostList, self.AfrList, self.BattList, self.AirList, self.FuelList], headers)
         print("save")
         #self.fileWrite(self.TempList, "temp.txt") 
         return
